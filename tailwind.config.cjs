5c60645a4720b91fb05350c49709e556aeb748bc
ranajaydas <ranajay@gmail.com>
2023-06-08 20:02:11 +0800

    Change order of sections and add clip circle

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,13 +2,16 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      "emoji-yellow": "rgb(var(--emoji-yellow) / <alpha-value>)",
+    },
+    extend: {
+      fontFamily: {
         sans: ["Epilogue", ...defaultTheme.fontFamily.sans],
       },
-		},
-	},
-	plugins: [],
-}
+    },
+  },
+  plugins: [],
+};
