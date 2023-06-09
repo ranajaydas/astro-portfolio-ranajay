63707442ba767fac5590cfadaae917c1240620b0
ranajaydas <ranajay@gmail.com>
2023-06-09 18:06:38 +0800

    Try and fix console errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +12,12 @@ export default defineConfig({
     }),
   ],
   output: "server",
-  adapter: vercel({
-    analytics: true,
-  }),
+  adapter: vercel({ analytics: true }),
+  vite: {
+    define: {
+      "import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID": JSON.stringify(
+        process.env.VERCEL_ANALYTICS_ID
+      ),
+    },
+  },
 });
