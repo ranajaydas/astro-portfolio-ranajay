c1381ef65fded01abcad0e73e25e8ab4780859ee
ranajaydas <ranajay@gmail.com>
2023-06-09 19:24:23 +0800

    Remove Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
-import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
   integrations: [
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
-  adapter: vercel({ analytics: true }),
-  vite: {
-    define: {
-      "import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID": JSON.stringify(
-        process.env.VERCEL_ANALYTICS_ID
-      ),
-    },
-  },
 });
