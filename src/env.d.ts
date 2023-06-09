4558353e987552a05a57a5fe17c8dc4a2267dd90
ranajaydas <ranajay@gmail.com>
2023-06-09 19:09:16 +0800

    Try to expose env variable

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="@astrojs/image/client" />
-console.log("VERCEL_ANALYTICS_ID", process.env.VERCEL_ANALYTICS_ID);
-console.log("VERCEL_ANALYTICS_ID", import.meta.env.VERCEL_ANALYTICS_ID);
-console.log(
-  "PUBLIC_VERCEL_ANALYTICS_ID",
-  process.env.PUBLIC_VERCEL_ANALYTICS_ID
-);
-console.log(
-  "PUBLIC_VERCEL_ANALYTICS_ID",
-  import.meta.env.PUBLIC_VERCEL_ANALYTICS_ID
-);
