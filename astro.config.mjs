3d636fb377917cc35b6e3d756b9d4053b2c8c1c6
ranajaydas <ranajay@gmail.com>
2023-07-24 16:11:12 +0800

    feat: vercel - deploy to edge with analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
+import vercel from '@astrojs/vercel/edge';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +12,8 @@ export default defineConfig({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
